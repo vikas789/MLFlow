03b91987efd0219b71f49d84d1de6423b798f99d
Vikas Bhargava <44107165+vikas789@users.noreply.github.com>
2022-09-30 13:57:16 +0530

    initial commit

---
+++ MobilePriceClassification.py
@@ -0,0 +1,33 @@
+import pandas as pd 
+import numpy as np
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm, tree
+import xgboost
+from sklearn.model_selection import train_test_split
+data = pd.read_csv('train.csv')
+x = data.drop(['price_range'],axis=1)
+y = data['price_range']
+X = (x- np.min(x)) / (np.max(x) - np.min(x))
+x_train,x_test, y_train, y_test = train_test_split(X,y, test_size=0.3, random_state=42)
+print(len(x_train))
+print(len(x_test))
+print(len(y_train))
+print(len(y_test))
+model1 = xgboost.XGBClassifier()
+model1.fit(x_train, y_train)
+y_pred= model1.predict(x_test)
+# print(y_pred)
+acc = accuracy_score(y_test, y_pred)
+print("Accuracy is %s"%(acc))
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix is %s"%(cm))
+
+model2 = RandomForestClassifier()
+model2.fit(x_train, y_train)
+y_pred= model2.predict(x_test)
+# print(y_pred)
+acc = accuracy_score(y_test, y_pred)
+print("Accuracy is %s"%(acc))
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix is %s"%(cm))
